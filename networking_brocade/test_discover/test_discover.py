e6b8e1af925859efdecc9982225f20be4fb224d6
Angela Smith <aallen@brocade.com>
2015-03-15 20:53:51 -0700

    Enabling MLX unit tests.
    Change-Id: Iad1ed4ef927a102fc4283ea9053d62255883e706

--- networking_brocade/test_discover/test_discover.py
+++ networking_brocade/test_discover/test_discover.py
@@ -28,7 +28,9 @@ def load_tests(loader, tests, pattern):
     base_path = os.path.split(os.path.dirname(os.path.abspath(__file__)))[0]
     base_path = os.path.split(base_path)[0]
     test_dirs = {'./networking_brocade/tests',
-                 './networking_brocade/vdx/tests/unit/ml2/drivers/brocade'}
+                 './networking_brocade/vdx/tests/unit/ml2/drivers/brocade',
+                 MLX_TEST_BASE_PATH + '/unit/ml2/drivers/brocade',
+                 MLX_TEST_BASE_PATH + '/unit/services/l3_router/brocade'}
     for test_dir in test_dirs:
         if not pattern:
             suite.addTests(loader.discover(test_dir, top_level_dir=base_path))
