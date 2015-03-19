03d86ae9c3ad858e471d823261b33d9876d5ca14
Pradeep Sathasivam <psathasi@brocade.com>
2015-03-19 18:27:11 +0530

    Unit test code added for ml2 and l3 services.
    Change-Id: I0a58695c57010590e1612d6aaa529fdacf15e1a0

--- networking_brocade/mlx/tests/unit/services/l3_router/brocade/test_brocade_l3_router_fi_ni_plugin.py
+++ networking_brocade/mlx/tests/unit/services/l3_router/brocade/test_brocade_l3_router_fi_ni_plugin.py
@@ -29,6 +29,9 @@ MECHANISM_NAME = ('networking_brocade.mlx.services.l3_router.brocade.'
                   'l3_router_fi_ni_plugin.BrocadeFiNiL3RouterPlugin')
 VE = 've '
 config_map = {}
+vlan_map = {}
+interface_map = {}
+
 device_map = {
     'mlx': {
         'address': '2.2.2.2',
@@ -52,6 +55,10 @@ delete_config_map = {
     }
 }
 
+interface_info = {
+    'subnet_id': 'subnet1',
+    'port_id': 'port1'}
+
 vlan_id = '402'
 gateway_ip_cidr = '12.0.0.1/24'
 ROUTER = 'router1'
@@ -73,131 +80,121 @@ class TestBrocadeL3RouterFiNiPlugin(base.BaseTestCase,
         _mechanism_name = MECHANISM_NAME
 
         def mocked_initialize(self):
-            self._router_devices_map = device_map
-            self._driver_map = {'mlx': FakeDriver(device_map.get('mlx'))}
+            self._devices = device_map
 
         with mock.patch.object(brocadel3routerfiniplugin
                                .BrocadeFiNiL3RouterPlugin,
                                'brocade_init', new=mocked_initialize):
             super(TestBrocadeL3RouterFiNiPlugin, self).setUp()
-            self.interface_info = {'subnet_id': SUBNET,
-                                   'port_id': PORT}
             self.driver = importutils.import_object(_mechanism_name)
 
-    @mock.patch.object(
-        brocadel3routerfiniplugin.BrocadeFiNiL3RouterPlugin,
-        '_get_network_info')
+    @mock.patch.object(brocadel3routerfiniplugin.BrocadeFiNiL3RouterPlugin,
+                       '_get_driver')
+    @mock.patch.object(brocadel3routerfiniplugin.BrocadeFiNiL3RouterPlugin,
+                       '_get_network_info')
     @mock.patch.object(l3_db.L3_NAT_dbonly_mixin, '_core_plugin')
-    @mock.patch.object(router.L3RouterPlugin,
-                       'add_router_interface')
+    @mock.patch.object(router.L3RouterPlugin, 'add_router_interface')
     def test_add_router_interface(
-            self, mock_super, mock_core_plugin, mock_network_info):
+        self, mock_super, mock_core_plugin, mock_network_info,
+            mock_driver):
 
-        mech_ctx = _get_network_context('physnet1', 'vlan')
+        mock_driver.side_effect = self.side_effect
+        mech_ctx = self._get_network_context('physnet1', 'vlan')
         ctx = mech_ctx._plugin_context
         ctx.session.begin = mock.MagicMock()
-        mock_super.returnValue = self.interface_info
+        mock_super.returnValue = interface_info
         mock_core_plugin.side_effect = mock.MagicMock()
-        mock_network_info.side_effect = side_effect_network_info
+        mock_network_info.side_effect = self.side_effect_network_info
         self.driver.add_router_interface(ctx, ROUTER,
-                                         self.interface_info)
-        global config_map
+                                         interface_info)
         self.assertDictSupersetOf(config_map, add_config_map)
 
-    @mock.patch.object(
-        brocadel3routerfiniplugin.BrocadeFiNiL3RouterPlugin,
-        '_get_network_info')
-    @mock.patch.object(l3_db.L3_NAT_dbonly_mixin, '_core_plugin')
-    @mock.patch.object(router.L3RouterPlugin, 'remove_router_interface')
-    def test_remove_router_interface(
-            self, mock_super, mock_core_plugin, mock_network_info):
-        mech_ctx = _get_network_context('physnet1', 'vlan')
-        ctx = mech_ctx._plugin_context
-        ctx.session.begin = mock.MagicMock()
-        mock_super.returnValue = self.interface_info
-        mock_core_plugin.side_effect = mock.MagicMock()
-        mock_network_info.side_effect = side_effect_network_info
-        self.driver.remove_router_interface(ctx, ROUTER,
-                                            self.interface_info)
-        global config_map
-        self.assertDictSupersetOf(config_map, delete_config_map)
-
-
-class TestBrocadeL3RouterFiNiPluginException(base.BaseTestCase,
-                                             router.L3RouterPlugin):
-
-    """
-    Test Brocade L3 Router FI/NI plugin for negative scenario
-    """
-
-    def setUp(self):
-        _mechanism_name = MECHANISM_NAME
-
-        def mocked_initialize(self):
-            self._router_devices_map = device_map
-            self._driver_map = {
-                'mlx': FakeDriver(
-                    device_map.get('mlx'),
-                    error=True)}
-
-        with mock.patch.object(brocadel3routerfiniplugin
-                               .BrocadeFiNiL3RouterPlugin,
-                               'brocade_init', new=mocked_initialize):
-            super(TestBrocadeL3RouterFiNiPluginException, self).setUp()
-            self.interface_info = {'subnet_id': SUBNET,
-                                   'port_id': PORT}
-            self.driver = importutils.import_object(_mechanism_name)
-
-    @mock.patch.object(
-        brocadel3routerfiniplugin.BrocadeFiNiL3RouterPlugin,
-        '_get_network_info')
+    @mock.patch.object(brocadel3routerfiniplugin.BrocadeFiNiL3RouterPlugin,
+                       '_get_driver')
+    @mock.patch.object(brocadel3routerfiniplugin.BrocadeFiNiL3RouterPlugin,
+                       '_get_network_info')
     @mock.patch.object(l3_db.L3_NAT_dbonly_mixin, '_core_plugin')
     @mock.patch.object(router.L3RouterPlugin, 'add_router_interface')
     def test_add_router_interface_exception(
-            self, mock_super, mock_core_plugin, mock_network_info):
+        self, mock_super, mock_core_plugin, mock_network_info,
+            mock_driver):
 
-        mech_ctx = _get_network_context('physnet1', 'vlan')
+        mock_driver.side_effect = self.side_effect_error
+        mech_ctx = self._get_network_context('physnet1', 'vlan')
         ctx = mech_ctx._plugin_context
         ctx.session.begin = mock.MagicMock()
-        mock_super.returnValue = self.interface_info
+        mock_super.returnValue = interface_info
         mock_core_plugin.side_effect = mock.MagicMock()
-        mock_network_info.side_effect = side_effect_network_info
+        mock_network_info.side_effect = self.side_effect_network_info
         self.assertRaisesRegexp(
             Exception, (_LE("BrocadeFiNiL3RouterPlugin")),
             self.driver.add_router_interface,
-            ctx, ROUTER, self.interface_info)
+            ctx, ROUTER, interface_info)
 
+    @mock.patch.object(brocadel3routerfiniplugin.BrocadeFiNiL3RouterPlugin,
+                       '_get_driver')
+    @mock.patch.object(brocadel3routerfiniplugin.BrocadeFiNiL3RouterPlugin,
+                       '_get_network_info')
+    @mock.patch.object(l3_db.L3_NAT_dbonly_mixin, '_core_plugin')
+    @mock.patch.object(router.L3RouterPlugin, 'remove_router_interface')
+    def test_remove_router_interface(
+        self, mock_super, mock_core_plugin, mock_network_info,
+            mock_driver):
 
-def _get_network_context(physnet, network_type):
-    """
-    Create mock network context
-    """
-    network = {
-        'id': 1,
-        'name': 'private',
-        'tenant_id': 1,
-        'vlan': 200,
-        'network_type': network_type,
-        'provider:segmentation_id': 200
-    }
+        mock_driver.side_effect = self.side_effect
+        mech_ctx = self._get_network_context('physnet1', 'vlan')
+        ctx = mech_ctx._plugin_context
+        ctx.session.begin = mock.MagicMock()
+        mock_super.returnValue = interface_info
+        mock_core_plugin.side_effect = mock.MagicMock()
+        mock_network_info.side_effect = self.side_effect_network_info
+        self.driver.remove_router_interface(ctx, ROUTER,
+                                            interface_info)
+        self.assertDictSupersetOf(config_map, delete_config_map)
 
-    network_segments = [{
-        'id': 1,
-        'segmentation_id': 200,
-        'network_type': network_type,
-        'physical_network': physnet
-    }]
+    def side_effect(self, dev_name):
+        """
+        Mock _get_driver method and return FakeDriver
+        """
+        device = device_map.get(dev_name)
+        return FakeDriver(device)
 
-    _plugin_context = context.get_admin_context()
-    return FakeNetworkContext(network, network_segments, _plugin_context)
+    def side_effect_error(self, dev_name):
+        """
+        Mock _get_driver method and return FakeDriver
+        """
+        device = device_map.get(dev_name)
+        return FakeDriver(device, error=True)
 
+    def _get_network_context(self, physnet, network_type):
+        """
+        Create mock network context
+        """
+        network = {
+            'id': 1,
+            'name': 'private',
+            'tenant_id': 1,
+            'vlan': 200,
+            'network_type': network_type,
+            'provider:segmentation_id': 200
+        }
 
-def side_effect_network_info(vlan, ip):
-    """
-    Mock _get_driver method and return FakeDriver
-    """
+        network_segments = [{
+            'id': 1,
+            'segmentation_id': 200,
+            'network_type': network_type,
+            'physical_network': physnet
+        }]
 
-    return vlan_id, gateway_ip_cidr
+        _plugin_context = context.get_admin_context()
+        return FakeNetworkContext(network, network_segments, _plugin_context)
+
+    def side_effect_network_info(self, vlan, ip):
+        """
+        Mock _get_driver method and return FakeDriver
+        """
+
+        return vlan_id, gateway_ip_cidr
 
 
 class FakeNetworkContext(object):
@@ -247,19 +244,13 @@ class FakeDriver(object):
                             "ve out of range error")
         elif self.error:
             raise Exception("Add Router Interface failed")
-        vlan_map = {}
-        interface_map = {}
+
         interface_map.update({VE + vlan_id: gateway_ip_cidr})
         vlan_map.update({vlan_id: interface_map})
-        global config_map
         config_map.update({self.address: vlan_map})
 
     def remove_router_interface(self, vlan_id):
-        vlan_map = delete_config_map.pop(self.address, None)
-        interface_map = vlan_map.pop(vlan_id, None)
-        if vlan_map is None:
-            raise Exception("No Address")
-        elif interface_map is None:
-            raise Exception("No Router Interface")
+        if vlan_id in config_map[self.address]:
+            config_map[self.address].update({vlan_id: {}})
         else:
-            interface_map.pop(VE + vlan_id, None)
+            raise Exception("vlan is not there")
