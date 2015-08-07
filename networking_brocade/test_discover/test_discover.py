a113174152703938102b0d782e0388038ed8dc8a
Pradeep Sathasivam <psathasi@brocade.com>
2015-08-07 14:49:48 +0530

    Support for enable password configuration
    When enable password is configured in device, ML2 plugin
    will not be able to configure the device via SSH or TELNET.
    
    Now this is supported by reading the response of each command
    and identifying the prompt.
    
    Change-Id: I3344f63b2f076809046fdbe92b9c9227bc0f1e1c
    Closes-Bug: 1481161

--- networking_brocade/test_discover/test_discover.py
+++ networking_brocade/test_discover/test_discover.py
@@ -28,10 +28,8 @@ def load_tests(loader, tests, pattern):
     base_path = os.path.split(os.path.dirname(os.path.abspath(__file__)))[0]
     base_path = os.path.split(base_path)[0]
     test_dirs = {'./networking_brocade/tests',
-                 './networking_brocade/vdx/tests/unit/ml2/drivers/brocade',
                  MLX_TEST_BASE_PATH + '/unit/ml2/drivers/brocade',
-                 MLX_TEST_BASE_PATH + '/unit/services/l3_router/brocade',
-                 }
+                 MLX_TEST_BASE_PATH + '/unit/services/l3_router/brocade'}
     for test_dir in test_dirs:
         if not pattern:
             suite.addTests(loader.discover(test_dir, top_level_dir=base_path))
