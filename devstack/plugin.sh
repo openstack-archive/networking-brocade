338d7017968bdd324f262dd44972109a0dc4f26c
Angela Smith <aallen@brocade.com>
2016-02-16 14:55:51 -0800

    Update Brocade MLX plugin from Neutron decomposition
    Add ini files to repository and list ini files and entry
    points in setup.cfg for MLX plugins.
    
    Update requirements.txt, tox.ini files for Neutron repo
    dependencies.
    
    Add config registrations to mech driver as these were
    removed when decomposed from Neutron.  Update mech driver
    to return in cases of unsupported type or physnet
    instead of throwing exception.
    
    Update requirements.txt to be same as current Neutron
    requirements.
    
    Add devstack plugin shell script.
    
    Change-Id: Id32efe9ea3580b559d86c1b8a1db7aeb4aa690d7

---
+++ devstack/plugin.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+DIR_BRCD=$DEST/networking-brocade
+
+if is_service_enabled net-brcd; then
+
+    if [[ "$1" == "source" ]]; then
+        :
+    fi
+
+    if [[ "$1" == "stack" && "$2" == "install" ]]; then
+        cd $DIR_BRCD
+        echo "Installing networking-brocade"
+        setup_develop $DIR_BRCD
+
+    if [[ "$1" == "clean" ]]; then
+        :
+    fi
+fi
