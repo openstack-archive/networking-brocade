dfc1628a5f58a3144d86f60621d2c86a22e04fcb
Angela Smith <aallen@brocade.com>
2016-02-16 14:55:51 -0800

    Update Brocade MLX plugin from Neutron decomposition
    Add ini files to repository and list ini files and entry
    points in setup.cfg for MLX plugins.
    
    Update requirements.txt, tox.ini files for Neutron repo
    dependencies.
    
    Add config registrations to mech driver as these were
    removed when decomposed from Neutron.  Update mech driver
    to return in cases of unsupported type or physnet
    instead of throwing exception.
    
    Update requirements.txt to be same as current Neutron
    requirements.
    
    Change-Id: Id32efe9ea3580b559d86c1b8a1db7aeb4aa690d7

--- networking_brocade/mlx/ml2/fi_ni/brcd_config.py
+++ networking_brocade/mlx/ml2/fi_ni/brcd_config.py
@@ -20,6 +20,57 @@ Parses the brocade ethernet configuration template
 
 from oslo_config import cfg
 
+SWITCHES = [
+    cfg.StrOpt(
+        'switch_names',
+        default='',
+        help=('Switches connected to the compute nodes'))]
+
+ML2_BROCADE = [cfg.StrOpt('address', default='',
+                          help=('The IP address of the MLX or ICX switch')),
+               cfg.StrOpt('username', default='admin',
+                          help=('The SSH username of the switch')),
+               cfg.StrOpt('password', default='password', secret=True,
+                          help=('The SSH password of the switch')),
+               cfg.StrOpt('physical_networks', default='',
+                          help=('Allowed physical networks where VLAN can '
+                                'be configured on this switch')),
+               cfg.StrOpt('ports', default='',
+                          help=('Ports to be tagged in the VLAN being '
+                                'configured on the switch')),
+               cfg.StrOpt('transport', default='SSH',
+                          choices=('SSH', 'TELNET'),
+                          help=('Protocol used to communicate with switch')),
+               cfg.StrOpt('ostype', default='NI', choices=('NI', 'FI'),
+                          help=('OS type of the device.  NI is NetIron '
+                                'for MLX switches. FI is FastIron for '
+                                'ICX switches.')),
+               cfg.StrOpt('enable_username', default='admin',
+                          help=('The username for enable config prompt')),
+               cfg.StrOpt('enable_password', default='password', secret=True,
+                          help=('The password for enable config prompt')),
+               ]
+L3_BROCADE = [cfg.StrOpt('address', default='',
+                         help=('The IP address of the MLX switch')),
+              cfg.StrOpt('username', default='admin',
+                         help=('The SSH username of the switch')),
+              cfg.StrOpt('password', default='password', secret=True,
+                         help=('The SSH password of the switch')),
+              cfg.StrOpt('physical_networks', default='',
+                         help=('Allowed physical networks where VLAN can '
+                               'be configured on this switch')),
+              cfg.StrOpt('ports', default='',
+                         help=('Ports to be tagged in the VLAN being '
+                               'configured on the switch')),
+              cfg.StrOpt('enable_username', default='admin',
+                         help=('The username for enable config prompt')),
+              cfg.StrOpt('enable_password', default='password', secret=True,
+                         help=('The password for enable config prompt')),
+              ]
+cfg.CONF.register_opts(SWITCHES, 'ml2_brocade_fi_ni')
+cfg.CONF.register_opts(ML2_BROCADE, 'ML2_BROCADE_MLX_EXAMPLE')
+cfg.CONF.register_opts(L3_BROCADE, 'L3_BROCADE_MLX_EXAMPLE')
+
 
 class ML2BrocadeConfig(object):
 
@@ -43,15 +94,21 @@ class ML2BrocadeConfig(object):
         :param:isL2: Boolean value based on the router type L2 or L3
         """
         if isL2:
+            cfg.CONF.register_opts(SWITCHES, 'ml2_brocade_fi_ni')
             self._brocade_switches = (cfg.CONF.ml2_brocade_fi_ni.
                                       switch_names)
         else:
+            cfg.CONF.register_opts(SWITCHES, 'l3_brocade_fi_ni')
             self._brocade_switches = (cfg.CONF.l3_brocade_mlx.
                                       switch_names)
         switches = self._brocade_switches.split(',')
         for switch in switches:
             switch_info = {}
             switch = switch.strip()
+            if isL2:
+                cfg.CONF.register_opts(ML2_BROCADE, switch)
+            else:
+                cfg.CONF.register_opts(L3_BROCADE, switch)
             for key, value in cfg.CONF._get(switch).items():
                 value = value.strip()
                 switch_info.update({key: value})
