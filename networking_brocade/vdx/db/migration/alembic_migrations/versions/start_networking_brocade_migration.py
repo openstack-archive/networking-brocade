80f0711c3c46d48aaef71d227b6e95fe607b2de9
Ritesh Madapurath <rmadapur@brocade.com>
2016-03-29 02:18:27 -0700

    Adding Non-ampp support for VDX
    Add support for NON-AMPP(VLAN) based networking on VDX switches.
    NON-AMPP based networking can be accessed through a seperate mechanism driver.
    
    Change-Id: I4e67ff816af71ebbed1be29023c8b3c652ab3f73

---
+++ networking_brocade/vdx/db/migration/alembic_migrations/versions/start_networking_brocade_migration.py
@@ -0,0 +1,34 @@
+# opyright 2015 OpenStack Foundation
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+#
+
+"""start networking-brocade chain
+
+Revision ID: start_networking_brocade_migration
+Revises: None
+Create Date: 2015-02-04 11:06:18.196062
+
+"""
+
+# revision identifiers, used by Alembic.
+revision = 'start_ml2_brcd'
+down_revision = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
